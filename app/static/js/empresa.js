bcf6c234346d823bbdb7f7fe146af54302253a7e
Jonas Nascimento <jonashnascimento@gmail.com>
2024-05-11 15:03:24 -0300

    adicionar todas as operacoes de CRUD de avaliacoes na pagina de empresa no frontend

--- app/static/js/empresa.js
+++ app/static/js/empresa.js
@@ -4,17 +4,196 @@ const id = params.get('id');
 
 if(id) {
     fetch("/api/empresas/" + id)
-    .then(response => {
-        if(response.ok)
-            return response.json()
-        else {
-            throw new Error('Erro ao buscar dados: ' + response.status);
-        }
-    }).then(data => {
-        const nome = document.getElementById("nome-empresa")
-        const setor = document.getElementById("setor-empresa")
+        .then(response => {
+            if(response.ok)
+                return response.json()
+            else {
+                throw new Error('Erro ao buscar dados: ' + response.status);
+            }
+        }).then(data => {
+            const nome = document.getElementById("nome-empresa")
+            const setor = document.getElementById("setor-empresa")
 
-        nome.textContent = data.nome
-        setor.textContent = data.setora
-    }).catch(error => console.log(error))
+            nome.textContent = data.nome
+            setor.textContent = data.setor
+        }).catch(error => console.log(error))
+
+        
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    renderAvaliacoes();
+});
+
+function renderAvaliacoes() {
+    const params = new URLSearchParams(window.location.search);
+    const empresaId = params.get('id');
+
+    const list = document.getElementById("avaliacoes-list")
+    list.innerHTML = ""
+
+    fetch(`/api/empresas/${empresaId}/avaliacoes`)
+        .then(response => {
+            if(response.ok)
+                return response.json()
+            else {
+                throw new Error('Erro ao buscar dados: ' + response.status);
+            }
+        }).then(data => {
+            if(data.length > 0) {
+                data.sort(compararPorId).forEach(avaliacao => {
+                    if(avaliacao.titulo && avaliacao.texto) {
+                        const li = document.createElement("li");
+                        li.className = "avaliacao-li"
+    
+                        const title = document.createElement("h3")
+                        title.textContent = avaliacao.titulo
+    
+                        const text = document.createElement("p")
+                        text.textContent = avaliacao.texto
+    
+                        li.appendChild(title)
+                        li.appendChild(text)
+    
+    
+                        const editButton = document.createElement("button")
+                        editButton.className = "edit-button"
+                        editButton.type = "button"
+                        editButton.textContent = "Editar"
+                        editButton.addEventListener("click", () => editarAvaliacao(li, empresaId, avaliacao.id, avaliacao.titulo, avaliacao.texto))
+    
+                        const deleteButton = document.createElement("button")
+                        deleteButton.className = "delete-button"
+                        deleteButton.type = "button"
+                        deleteButton.textContent = "Excluir"
+                        deleteButton.addEventListener("click", () => deletarAvaliacao(empresaId, avaliacao.id))
+    
+                        const buttonsdiv = document.createElement("div")
+                        buttonsdiv.className = "avaliacao-buttons-div"
+    
+                        buttonsdiv.appendChild(editButton)
+                        buttonsdiv.appendChild(deleteButton)
+    
+                        li.appendChild(buttonsdiv)
+    
+                        list.appendChild(li)
+                    }
+                });
+            }
+        }).catch(error => console.log(error))
+}
+
+function addAvaliacao() {
+    const params = new URLSearchParams(window.location.search);
+    const empresaId = params.get('id');
+    
+    const titulo = document.querySelector("#avaliacao-div > #titulo")
+    const texto = document.querySelector("#avaliacao-div > #texto")
+    const avaliacao = {
+        titulo : titulo.value,
+        texto : texto.value
+    }
+    fetch(`/api/empresas/${empresaId}/avaliacoes`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(avaliacao),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao enviar o formulário');
+            } 
+            
+            titulo.value = ""
+            texto.value = ""
+            renderAvaliacoes()
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
+}
+
+function editarAvaliacao(li, empresaId, avaliacaoId, titulo, texto) {
+    li.innerHTML = ""
+    
+    const tituloLabel = document.createElement("span")
+    tituloLabel.textContent = "Digite um titulo: "
+
+    const tituloInput = document.createElement("input")
+    tituloInput.className = "edit-titulo-input"
+    tituloInput.value = titulo
+
+    const textLabel = document.createElement("span")
+    textLabel.textContent = "Digite um texto: "
+
+    const textInput = document.createElement("textarea")
+    textInput.className = "edit-text-input"
+    textInput.value = texto
+
+    const editTileButtons = document.createElement("div")
+    editTileButtons.className = "edit-tile-buttons"
+
+    const saveButton = document.createElement("button")
+    saveButton.type = "button"
+    saveButton.textContent = "Salvar"
+    saveButton.addEventListener("click", () => {
+        const avaliacao = {
+            titulo : tituloInput.value,
+            texto : textInput.value
+        }
+        if((titulo!==avaliacao.titulo) || (texto!==avaliacao.texto)) {
+            fetch(`/api/empresas/${empresaId}/avaliacoes/${avaliacaoId}`, {
+                method: 'PUT',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(avaliacao),
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Erro ao enviar o formulário');
+                    } 
+                    
+                    titulo.value = ""
+                    texto.value = ""
+                    renderAvaliacoes()
+                })
+                .catch(error => {
+                    console.error('Erro:', error);
+                });
+        }
+    } )
+
+    const cancelButton = document.createElement("button")
+    cancelButton.type = "button"
+    cancelButton.textContent = "Cancelar"
+
+    editTileButtons.appendChild(saveButton)
+    editTileButtons.appendChild(cancelButton)
+
+    li.appendChild(tituloLabel)
+    li.appendChild(tituloInput)
+    li.appendChild(textLabel)
+    li.appendChild(textInput)
+    li.appendChild(editTileButtons)
+}
+
+function deletarAvaliacao(empresaId, avaliacaoId) {
+    fetch(`/api/empresas/${empresaId}/avaliacoes/${avaliacaoId}`, {
+        method: 'DELETE'
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao enviar o formulário');
+            } 
+            else renderAvaliacoes()
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
+}
+
+function compararPorId(a, b) {
+    return b.id - a.id;
 }
