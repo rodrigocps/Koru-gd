f7bd05711450b37d26376b2ab9138d1d2be60ef1
Jonas Nascimento <jonashnascimento@gmail.com>
2024-05-13 11:43:28 -0300

    estilizar lista de empresas e adicionar logo's

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -25,25 +25,39 @@ function fetchEmpresas (pagina, search) {
 
 function renderEmpresas(data) {
     if(data.length > 0) {
-        const listaDiv = document.querySelector("ul.lista-empresas")
+        const listaDiv = document.querySelector("div.lista")
         listaDiv.innerHTML = ""
+
+        const row = document.createElement("div")
+        row.classList.add("empresas-row", "row", "row-cols-1", "row-cols-lg-2", "row-cols-xl-2")
     
         data.forEach(empresa => {
-            const elem = getListElement(empresa)
+            const elem = getColElement(empresa)
             if(elem)
-                listaDiv.appendChild(elem)
+                row.appendChild(elem)
         });
+
+        listaDiv.appendChild(row)
     }
 }
 
-function getListElement(empresa) {
+function getColElement(empresa) {
     if (empresa.nome && empresa.id) {
-        const li = document.createElement("li");
-        li.className = "empresa-li"
+        const col = document.createElement("div");
+        col.classList.add("empresa-col", "col")
 
         const a = document.createElement("a")
         a.className = "empresa-element"
         a.href = "/empresas?id=" + empresa.id
+
+        const empresaLogo = document.createElement("img")
+        empresaLogo.classList.add("empresa-logo")
+        empresaLogo.alt = "Logo da empresa"
+        if(empresa.logo_url) empresaLogo.src = empresa.logo_url
+
+        const nomeContainer = document.createElement("div")
+        nomeContainer.className = "empresa-nome-container"
+
 
         const empresaName = document.createElement("span")
         empresaName.className = "empresa-name"
@@ -53,12 +67,15 @@ function getListElement(empresa) {
         empresaSetor.className = "empresa-setor"
         empresaSetor.textContent =  empresa.setor;
 
-        a.appendChild(empresaName)
-        a.appendChild(empresaSetor)
-        li.appendChild(a)
+        
+        nomeContainer.appendChild(empresaName)
+        nomeContainer.appendChild(empresaSetor)
+        a.appendChild(empresaLogo)
+        a.appendChild(nomeContainer)
+        col.appendChild(a)
 
     
-        return li;
+        return col;
     }
     return undefined
 }
