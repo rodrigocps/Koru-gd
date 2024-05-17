2201d93d90ccb3a7536a8fd0d8f30517ec222fac
Jonas Nascimento <jonashnascimento@gmail.com>
2024-05-17 17:41:13 -0300

    adicionar psycopg2 ao requirements.txt

--- app/config.py
+++ app/config.py
@@ -6,5 +6,3 @@ class Config:
     ...
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'database.db')
-
-    # SQLALCHEMY_DATABASE_URI = "postgresql://korujobsdb_user:3mUWHnfdkmECfd7omsXnFcSptem0SJDQ@dpg-cp17shn79t8c73a3nkcg-a.oregon-postgres.render.com/korujobsdb"
